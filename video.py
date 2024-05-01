7e8127dd96ec353de4c750025fbed6e995ca8c73
George-Sakellariou <168576831+George-Sakellariou@users.noreply.github.com>
2024-05-01 13:31:08 +0300

    Update video.py

--- video.py
+++ video.py
@@ -7,34 +7,35 @@ from echonet import datasets
 
 sys.path.append('/path/to/file') #paths are different for the demonstration
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #enable gpu cuda accelarator if possible
 
-mean, std = echonet.utils.get_mean_and_std(echonet.datasets.Echo(split="train"))
+mean, std = echonet.utils.get_mean_and_std(echonet.datasets.Echo(split="train")) #MAE and STD from the dataset based on echonet algorithm
 
-test_dataset = echonet.datasets.Echo(split = 'val', target_type = 'EF', clips = 'all', mean = mean, std = std, length = 32, period = 2, max_length = 250, pad =None)
+test_dataset = echonet.datasets.Echo(split = 'val', target_type = 'EF', clips = 'all', mean = mean, std = std, length = 32, period = 2, max_length = 250, pad =None) #set test dataset
 
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, num_workers = 1, shuffle = False, pin_memory = False)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, num_workers = 1, shuffle = False, pin_memory = False) #load test dataset
 
-input = next(iter(test_dataloader))
+input = next(iter(test_dataloader)) #initialize iteration for the dataset
 
-model = torchvision.models.video.__dict__['deepLabV3'](pretrained=False)
+model = torchvision.models.video.__dict__['deepLabV3'](pretrained=False) #set torchvision model for video analysis
 
-model.fc = torch.nn.Linear(model.fc.in_features, 1)
+model.fc = torch.nn.Linear(model.fc.in_features, 1) #set activation function for the nn model
 
-model = torch.nn.DataParallel(model)
+model = torch.nn.DataParallel(model) #set data parallelism at the module level
 
-onnxin = input[0]
+onnxin = input[0] #start iteration
 
-model.to(device)
+model.to(device) #pass model to device for conversion
 
-weights = '/home/georgios/dynamic/output/video/deepLabV3_pretrained/checkpoint.pt'
+weights = '/home/georgios/dynamic/output/video/deepLabV3_pretrained/checkpoint.pt' #set initial weights for the nn model
 
 checkpoint = torch.load(weights)
     
-model.load_state_dict(checkpoint['state_dict'])
+model.load_state_dict(checkpoint['state_dict']) #load weights
 
-model.eval()
+model.eval() #convert model to inference mode after training
 
+#create onnx inference model
 torch.onnx.export(model.module, 
                       
                 onnxin[:,0,...].to(device),
